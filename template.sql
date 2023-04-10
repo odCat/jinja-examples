6aee6291405ef180f789faa9cda3a7a33ca01321
Mihai Gătejescu <gus666xe@gmail.com>
2023-04-11 01:40:06 +0300

    Add a group by statement

--- template.sql
+++ template.sql
@@ -4,4 +4,7 @@ where 1=1
 {%- for condition in variable %}
    and {{ condition -}}
 {% endfor %}
+group by
+{%- for condition in variable %} {{ condition -}} {% if not loop.last %}, {%- endif %}
+{%- endfor -%}
 ;
